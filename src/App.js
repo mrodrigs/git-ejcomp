f961ae71c5b75e56c2838dc6ee2b837080eac883
Mauricio Rodrigues <grtorres17@gmail.com>
2020-08-17 20:09:13 -0300

    feat: display ejcomp logo

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="container">
+      <img className="logo" src={Logo} alt="Logo" />
     </div>
   );
 }
